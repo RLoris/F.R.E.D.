13f0762e0c5d9541146ce3b1a53926268af779a0
Rossi Loris <rossi.loris1998@gmail.com>
2019-04-26 23:45:38 +0200

    Adding main screen with widgets

--- client-angular/controlcenter/src/app/app-routing.module.ts
+++ client-angular/controlcenter/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverlayComponent } from './overlay/overlay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component : OverlayComponent},
+  { path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular/controlcenter/src/app/app.module.ts
+++ client-angular/controlcenter/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { EnvComponent } from './env/env.component';
+
+import {MatCardModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { HealthComponent } from './health/health.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverlayComponent,
+    EnvComponent,
+    HealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

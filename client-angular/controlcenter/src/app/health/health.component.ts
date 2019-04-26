00078054ebb2a8bd9b4f379993cf0ce4171b7ad7
Massimo Gentile <massimo.gentile@condorcet.be>
2019-04-27 01:33:05 +0200

    push

--- client-angular/controlcenter/src/app/health/health.component.ts
+++ client-angular/controlcenter/src/app/health/health.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
+import MiBand from 'miband';
+
 
 @Component({
   selector: 'app-health',
@@ -9,7 +12,24 @@ export class HealthComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // @ts-ignore
+    const bluetooth = navigator.bluetooth;
+
+    const device = await bluetooth.requestDevice({
+      filters: [
+        { services: [MiBand.advertisementService] }
+      ],
+      optionalServices: MiBand.optionalServices
+    });
+
+    const server = await device.gatt.connect();
+
+    const miband = new MiBand(server);
+    await miband.init();
+
+    log('Notifications demo...')
+    await miband.showNotification('message');
   }
 
 }

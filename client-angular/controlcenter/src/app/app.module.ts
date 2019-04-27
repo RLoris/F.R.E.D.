293e008b26af0f042ba2acd3b638d48fa9b9019b
Melissa <melissa.fontesse@hotmail.com>
2019-04-27 23:45:37 +0200

    Fixing merge issues

--- client-angular/controlcenter/src/app/app.module.ts
+++ client-angular/controlcenter/src/app/app.module.ts
@@ -25,7 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
     HealthComponent,
     ChillMusicVideoComponent,
     RelaxAndBreatheComponent,
-    EarthComponent
+    EarthComponent,
     FredComponent
   ],
   imports: [

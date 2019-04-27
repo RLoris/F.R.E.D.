400aa5b08bcfe4b642e100a6caf63d87db899acb
Guillaume <guillaumerigaux@hotmail.be>
2019-04-27 02:19:08 +0200

    Adding meteo component

--- client-angular/controlcenter/src/app/env/env.component.ts
+++ client-angular/controlcenter/src/app/env/env.component.ts
@@ -1,3 +1,5 @@
+import { SCENARIO } from './mock-env';
+import { Meteo } from './meteo';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnvComponent implements OnInit {
 
+  public meteos: Meteo[] = [];
+  public meteo: Meteo;
+
   constructor() { }
 
   ngOnInit() {
+    this.meteos = SCENARIO;
+    this.meteo = this.meteos[Math.floor(Math.random() * this.meteos.length)];
   }
 
 }
---
+++ client-angular/controlcenter/src/app/env/meteo.ts
@@ -0,0 +1,10 @@
+export class Meteo {
+
+  day: number;
+  tempMin: string;
+  tempMax: string;
+  tempMoy: string;
+  date: string;
+  vent: string;
+  pression: string;
+}
